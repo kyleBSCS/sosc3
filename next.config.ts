64fa2962f217160d795ebd1678035fac0807a925
kyleBSCS <kpvinuya@up.edu.ph>
2025-05-30 14:14:26 +0800

    chore: filled the remote patterns for added pictures to allow public urls

--- next.config.ts
+++ next.config.ts
@@ -55,6 +55,74 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "sam.nmartmuseum.org",
       },
+      {
+        protocol: "https",
+        hostname: "images.theconversation.com",
+      },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media-cldnry.s-nbcnews.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.blackpast.org",
+      },
+      {
+        protocol: "https",
+        hostname: "obamawhitehouse.archives.gov",
+      },
+      {
+        protocol: "https",
+        hostname: "blogs.publishersweekly.com",
+      },
+      {
+        protocol: "https",
+        hostname: "static01.nyt.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.nypl.org",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.squarespace-cdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "humanrightsconnected.org",
+      },
+      {
+        protocol: "https",
+        hostname: "daily.jstor.org",
+      },
+      {
+        protocol: "https",
+        hostname: "static1.squarespace.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.penguin.co.uk",
+      },
+      {
+        protocol: "https",
+        hostname: "fractals.blackfeministfuture.org",
+      },
+      {
+        protocol: "https",
+        hostname: "www.penguin.co.uk",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.britannica.com",
+      },
     ],
   },
 };
